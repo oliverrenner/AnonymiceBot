63429656c076637b6a14679ee21f7362ac8e2f8f
slingncrypto <mail@slingncrypto.eth>
2021-12-16 12:18:34 -0500

    update stats api to use settings and defined role ids in queries

--- src/controllers/statsApiController.js
+++ src/controllers/statsApiController.js
@@ -1,8 +1,20 @@
 const logger = require("../utils/logger");
+const config = require("../config");
+const fs = require("fs");
+const path = require("path")
+let useDevSettings = false;
+if(config.envName === "development" && fs.existsSync(__dirname, "../../settings.development.js")) {
+  useDevSettings = true;
+}
+const settings = useDevSettings ? require("../../settings.development") : require("../../settings");
 const User = require("../db/models/user");
 const VerificationRequest = require("../db/models/verificationRequest");
 
 class StatsController {
+  constructor() {
+    let verificationConfig = settings.rules.find(r => r.name === "Anonymice Verifier");
+    this.roleConfiguration = verificationConfig.executor.config.roles;
+  }
   async getTotal(req, res) {
     const result = await User.count().exec();
     res
@@ -14,8 +26,9 @@ class StatsController {
   }
 
   async getGenesis(req, res) {
+    let roleId = this.roleConfiguration.find(r => r.name === "Genesis Mice").id;
     const result = await User.count({
-      status: { $elemMatch: { role: "Genesis Mice", qualified: true } },
+      status: { $elemMatch: { roleId: roleId, qualified: true } },
     });
     res
       .status(200)
@@ -26,8 +39,9 @@ class StatsController {
   }
 
   async getBabies(req, res) {
+    let roleId = this.roleConfiguration.find(r => r.name === "Baby Mice").id;
     const result = await User.count({
-      status: { $elemMatch: { role: "Baby Mice", qualified: false } },
+      status: { $elemMatch: { roleId: roleId, qualified: true } },
     });
     res
       .status(200)
